65f6f834f9affdf723a416f4975f947a056d8255
mohamad <mh711748@gmail.com>
2025-05-20 17:15:21 +0330

    Result-Type

--- src/main.rs
+++ src/main.rs
@@ -10,12 +10,23 @@ fn print_verified(account: &BankAcount) {
     println!("{:?}", account.verified)
 }
 
+fn is_verified(account: &BankAcount) -> Result<bool, bool> {
+    return match account.verified {
+        true => Ok(true),
+        false => Err(false),
+    };
+}
+
 fn main() {
     let mohamad_account = BankAcount {
         balance: 10000,
-        verified: true,
+        verified: false,
     };
+
+    let verification_status = is_verified(&mohamad_account).expect("unable to unwrap result");
 
     print_balance(&mohamad_account);
     print_verified(&mohamad_account);
+
+    println!("{:?}", verification_status)
 }
