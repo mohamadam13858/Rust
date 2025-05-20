2355fda78348d58a77d576a96455db4e12f22ced
mohamad <mh711748@gmail.com>
2025-05-20 16:46:40 +0330

    ownership

--- src/main.rs
+++ src/main.rs
@@ -3,16 +3,19 @@ struct BankAcount {
     verified: bool,
 }
 
+fn print_balance(account: &BankAcount) {
+    println!("{:?}", account.balance)
+}
+fn print_verified(account: &BankAcount) {
+    println!("{:?}", account.verified)
+}
+
 fn main() {
     let mohamad_account = BankAcount {
         balance: 10000,
         verified: true,
     };
-    let ali = BankAcount {
-        balance: 20000000,
-        verified: false,
-    };
 
-    println!("{:?}", ali.balance)
+    print_balance(&mohamad_account);
+    print_verified(&mohamad_account);
 }
-    
