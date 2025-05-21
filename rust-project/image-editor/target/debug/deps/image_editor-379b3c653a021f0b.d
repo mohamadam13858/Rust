d0629f34513db95fcb990264b195f2e27172a5e9
mohamad <mh711748@gmail.com>
2025-05-21 19:15:37 +0330

    image-editor

---
+++ rust-project/image-editor/target/debug/deps/image_editor-379b3c653a021f0b.d
@@ -0,0 +1,5 @@
+C:\Users\moham\Desktop\Rust1\rust-project\image-editor\target\debug\deps\libimage_editor-379b3c653a021f0b.rmeta: src\lib.rs
+
+C:\Users\moham\Desktop\Rust1\rust-project\image-editor\target\debug\deps\image_editor-379b3c653a021f0b.d: src\lib.rs
+
+src\lib.rs:
---
+++ rust-project/image-editor/target/debug/deps/image_editor-9832c6f1f64ab8e7.d
@@ -0,0 +1,5 @@
+C:\Users\moham\Desktop\Rust1\rust-project\image-editor\target\debug\deps\libimage_editor-9832c6f1f64ab8e7.rmeta: src\lib.rs
+
+C:\Users\moham\Desktop\Rust1\rust-project\image-editor\target\debug\deps\image_editor-9832c6f1f64ab8e7.d: src\lib.rs
+
+src\lib.rs:
